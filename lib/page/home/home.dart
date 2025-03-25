682faa5b63f9452cb196349fc5366f05389355e7
ilsong963 <ilsong963@naver.com>
2025-03-25 19:25:40 +0900

    feat: home Page layout

--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/page/home/widget/station_select.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('기차 예매'), centerTitle: true));
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(title: Text('기차 예매'), centerTitle: true),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.white),
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  StationSelect(stationType: "출발역"),
+                  Container(height: 50, width: 2, color: Colors.grey[200]),
+                  StationSelect(stationType: "도착역"),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white, //change background color of button
+                  backgroundColor: Colors.purple,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+
+                  textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                child: Text('좌석 선택'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
