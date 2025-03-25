dcc6cf693730e33671192e75ed509bf8c7056b3e
ilsong963 <ilsong963@naver.com>
2025-03-25 20:47:26 +0900

    feat: stationListPage route

--- lib/page/home/widget/station_select.dart
+++ lib/page/home/widget/station_select.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/page/statin_list/station_list_page.dart';
 
 class StationSelect extends StatelessWidget {
   const StationSelect({super.key, required this.stationType});
@@ -11,7 +12,12 @@ class StationSelect extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(stationType, style: TextStyle(fontSize: 16, color: Colors.grey, fontWeight: FontWeight.bold)),
-        TextButton(onPressed: () {}, child: Text(station ?? "선택", style: TextStyle(fontSize: 40, color: Colors.black))),
+        TextButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => StationListPage(title: stationType)));
+          },
+          child: Text(station ?? "선택", style: TextStyle(fontSize: 40, color: Colors.black)),
+        ),
       ],
     );
   }
