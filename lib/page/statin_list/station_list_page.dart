17b4936ce71b503d86fc60a7c3f7912d24a34f38
ilsong963 <ilsong963@naver.com>
2025-03-25 20:46:58 +0900

    feat: add stationListPage

--- lib/page/statin_list/station_list_page.dart
+++ lib/page/statin_list/station_list_page.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class StationListPage extends StatelessWidget {
+  StationListPage({super.key, required this.title});
+  final List<String> stationList = ["수서", "동탄", "평택지제", "천안아산", "오송", "대전", "김천구미", "동대구", "경주", "울산", "부산"];
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:
+            stationList.map((station) {
+              return Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey[300]!))),
+                alignment: Alignment.centerLeft,
+                child: Text(station, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              );
+            }).toList(),
+      ),
+    );
+  }
+}
