b1fdceb980b0c8fe7dcf4f212c10586ff56defa8
ilsong963 <ilsong963@naver.com>
2025-03-26 10:47:28 +0900

    feat: seatPage 레이아웃

--- lib/page/seat_select/seat_page.dart
+++ lib/page/seat_select/seat_page.dart
@@ -5,6 +5,136 @@ class SeatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('좌석 선택'), centerTitle: true),
+
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "출발역",
+                  style: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(Icons.arrow_circle_right_outlined, size: 30),
+                Text(
+                  "도착역",
+                  style: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                seat(24, Colors.purple),
+                SizedBox(width: 4),
+                Text('선택됨'),
+                SizedBox(width: 20),
+                seat(24, Colors.grey[300]!),
+                SizedBox(width: 4),
+                Text('선택안됨'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:
+                  ['A', 'B', ' ', 'C', 'D']
+                      .map<Widget>(
+                        (element) => Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 2),
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Center(
+                              child: Text(
+                                element,
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return row(index + 1);
+                },
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SeatPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor:
+                      Colors.white, //change background color of button
+                  backgroundColor: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                child: Text('예매 하기'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding row(int index) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          seat(50, Colors.grey),
+          SizedBox(width: 4),
+          seat(50, Colors.grey),
+          SizedBox(width: 4),
+          seat(50, Colors.white, text: index.toString()),
+          SizedBox(width: 4),
+          seat(50, Colors.grey),
+          SizedBox(width: 4),
+          seat(50, Colors.grey),
+        ],
+      ),
+    );
+  }
+
+  Widget seat(double size, Color color, {String? text}) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      width: size,
+      height: size,
+      child: text == null ? SizedBox() : Center(child: Text(text.toString())),
+    );
   }
 }
