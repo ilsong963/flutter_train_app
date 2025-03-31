647626d3adef569c805b05ff5a127bd442a17a51
ilsong963 <ilsong963@naver.com>
2025-03-31 21:42:43 +0900

    feat: 예매 내역위젯 수정

--- lib/page/train_reservation_detail/train_reservation_detail_page.dart
+++ lib/page/train_reservation_detail/train_reservation_detail_page.dart
@@ -1,30 +1,110 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_train_app/model/train_booking_model.dart';
 import 'package:flutter_train_app/value_notifier/train_reservation_value_notifier.dart';
+import 'package:go_router/go_router.dart';
 
 class TrainReservationDetailPage extends StatelessWidget {
   const TrainReservationDetailPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<List<TrainReservationModel>>(
-      valueListenable: TrainReservationValueNotifier.trainReservationList,
-      builder: (context, items, child) {
-        return ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: Column(
-                children: [
-                  Text(
-                    "${items[index].startingStation}  -> ${items[index].destinationStation} ",
+    return Scaffold(
+      appBar: AppBar(title: Text('예매 내역'), centerTitle: true),
+      body: SafeArea(
+        child: ValueListenableBuilder<List<TrainReservationModel>>(
+          valueListenable: TrainReservationValueNotifier.trainReservationList,
+          builder: (context, items, child) {
+            return ListView.builder(
+              padding: EdgeInsets.all(16),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final reservation = items[index];
+
+                return Container(
+                  margin: EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).cardColor,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.1),
+                        blurRadius: 8,
+                        spreadRadius: 2,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
                   ),
-                  Text(" 좌석 :${items[index].seatModel.getSeatString()} "),
-                ],
-              ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${reservation.startingStation} → ${reservation.destinationStation}",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            Text(
+                              "좌석: ${reservation.seatModel.getSeatString()}",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.purple[100],
+
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            reservationActionButton("수정하기", () {}),
+
+                            Container(
+                              height: 30,
+                              width: 2,
+                              color: Colors.white,
+                            ),
+                            reservationActionButton("취소하기", () {}),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             );
           },
-        );
-      },
+        ),
+      ),
+    );
+  }
+
+  Widget reservationActionButton(String str, Function() onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          str,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+      ),
     );
   }
 }
